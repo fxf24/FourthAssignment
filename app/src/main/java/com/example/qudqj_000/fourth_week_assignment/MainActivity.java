b6633e6ac82407956dfcb23925f15b9eb644e1cb
Sam Kim <fxf24@naver.com>
2017-03-23 15:08:22 +0900

    Start set first page

---
+++ app/src/main/java/com/example/qudqj_000/fourth_week_assignment/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.qudqj_000.fourth_week_assignment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1, b2;
+    Intent intent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("4주차 실습");
+
+        program();
+    }
+
+    void program(){
+        b1 = (Button)findViewById(R.id.first_assignment);
+        b2 = (Button)findViewById(R.id.second_assignment);
+    }
+
+    public void OnClick(View v){
+        switch (v.getId()){
+            case R.id.first_assignment:
+                intent = new Intent(MainActivity.this, MenuActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.second_assignment:
+                intent = new Intent(MainActivity.this, CalcActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+}
